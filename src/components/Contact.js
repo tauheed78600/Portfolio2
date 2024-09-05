918ec904f8649af7af3e22db64c034f5091c0798
tauheed78600 <tauheeddarekar786@gmail.com>
2024-09-05 17:54:08 +0530

    changed to spec chars

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import '../styles/Contact.css';
-import axios from 'axios';
-import Footer from "../components/Footer.js"
+const React = require('react');
+const { useState } = require('react');
+require('../styles/Contact.css');
+const axios = require('axios');
+const Footer = require("../components/Footer.js");
+
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -49,7 +51,7 @@ const ContactForm = () => {
               <input required placeholder="Enter your Email" type="email" name="email" onChange={handleChange} />
             </div>
             <div className="input-box">
-              <input required placeholder="Enter your Phone 987yhgfr45tytrewsxfgt5432ert number" type="number" name="number" onChange={handleChange} />
+              <input required placeholder="Enter your Phone =-';/.,` number" type="number" name="number" onChange={handleChange} />
             </div>
             <div className="input-box">
               <input required placeholder="Enter your Message" type="text" name="text" onChange={handleChange} />
