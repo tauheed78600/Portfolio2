8594ca29a1c71d9ef96af85d142271774e6fe75a
tauheed78600 <tauheeddarekar786@gmail.com>
2024-09-05 17:28:32 +0530

    changes done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,7 +49,7 @@ const ContactForm = () => {
               <input required placeholder="Enter your Email" type="email" name="email" onChange={handleChange} />
             </div>
             <div className="input-box">
-              <input required placeholder="Enter your Phone please number" type="number" name="number" onChange={handleChange} />
+              <input required placeholder="Enter your Phone gcivgbk number" type="number" name="number" onChange={handleChange} />
             </div>
             <div className="input-box">
               <input required placeholder="Enter your Message" type="text" name="text" onChange={handleChange} />
